df7d953aa0dc66674da28a81190ab0809c4e36c4
softsolution123 <softsolutions.git@gmail.com>
2021-08-10 14:47:44 +0500

    dummy

--- Admin/Admin/Silkways/Models/Model1.Designer.cs
+++ Admin/Admin/Silkways/Models/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Zeeshan\SoftSolutions Projects\Updated Data\Gaint Shoe Box\Admin\Admin\Silkways\Models\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\github project\Giant Shoe Box Proejct Admin\Gaint-Shoe-box-admin\Admin\Admin\Silkways\Models\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
